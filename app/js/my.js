853172c3133a4c58dfcbc0f7c0b43fa4fc65008d
padilo300 <padilo300@gmail.com>
2018-12-27 18:35:20 +0200

    commit

--- app/js/my.js
+++ app/js/my.js
@@ -1,30 +1,22 @@
 $(document).ready(function () {
 
     
-    /////////////////////////////////////////////////////////
-    /////                                               /////
-    ///// Запуск счетчика цифр при скролле              /////   
-    /////                                               /////
-    /////////////////////////////////////////////////////////
-    
-    var show = true;
-    var countbox = "#counts";
-    $(window).on("scroll load resize", function () {
-        if (!show) return false;                      // Отменяем показ анимации, если она уже была выполнена
-        var w_top       = $(window).scrollTop()     ; // Количество пикселей на которое была прокручена страница
-        var e_top       = $(countbox).offset().top  ; // Расстояние от блока со счетчиками до верха всего документа
-        var w_height    = $(window).height()        ; // Высота окна браузера
-        var d_height    = $(document).height()      ; // Высота всего документа
-        var e_height    = $(countbox).outerHeight() ; // Высота всего документа
-        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-            $(".benefits__number").spincrement();
-            // $('.benefits__number').css('opacity', '1');
-            // $('.benefits__number').spincrement({
-            //     thousandSeparator: "",
-            //     duration: 1200
-            // });
-            show = false;
-        }
+  
+    $("#content-slider").lightSlider({
+        loop:true,
+        keyPress:true
+    });
+    $('#image-gallery').lightSlider({
+        gallery:true,
+        item:1,
+        thumbItem:9,
+        slideMargin: 0,
+        speed:500,
+        auto:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#image-gallery').removeClass('cS-hidden');
+        }  
     });
 
 });
