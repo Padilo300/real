4885c1f7f137dfde3105e25c8c95f623086fab5b
padilo300 <padilo300@gmail.com>
2019-01-04 10:13:07 +0200

    5 страницу начинаю

---
+++ adm/js/my.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    
+  
+    $("#content-slider").lightSlider({
+        loop:true,
+        keyPress:true
+    });
+    $('#image-gallery').lightSlider({
+        gallery:true,
+        item:1,
+        thumbItem:9,
+        slideMargin: 0,
+        speed:500,
+        auto:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#image-gallery').removeClass('cS-hidden');
+        }  
+    });
+
+});
